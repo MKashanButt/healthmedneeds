5a9978d3ce536cd37f4ed468a8a260b5eac49176
MKashanButt <kashanbutt566@gmail.com>
2025-07-15 00:59:53 +0500

    add relation to companies table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('company_id')
+                ->nullable()
+                ->constrained('companies')
+                ->nullOnDelete();
             $table->rememberToken();
             $table->timestamps();
         });
