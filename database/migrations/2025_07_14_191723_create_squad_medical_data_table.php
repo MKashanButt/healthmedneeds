a7ce3ca931ef9a09310db9fbebf6c746346fb0f3
MKashanButt <kashanbutt566@gmail.com>
2025-07-15 01:00:10 +0500

    add squad_medical_supply data table

---
+++ database/migrations/2025_07_14_191723_create_squad_medical_data_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('squad_medical_data', function (Blueprint $table) {
+            $table->id();
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('phone');
+            $table->date('dob');
+            $table->string('zip_code')
+                ->nullable();
+            $table->text('message')
+                ->nullable();
+            $table->text('ip')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('squad_medical_data');
+    }
+};
